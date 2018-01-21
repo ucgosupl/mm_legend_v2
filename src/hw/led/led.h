debb44ae6f078a20f83a6776a28927bcd27d9f4c
Maciej Gajdzica <mgajdzica@gmail.com>
2018-01-21 23:39:06 +0100

    led: add led driver implementation.

--- src/hw/led/led.h
+++ src/hw/led/led.h
@@ -7,20 +7,20 @@
 #ifndef _LED_H_
 #define _LED_H_
 
-/** Channel used by side left IR LED. */
-#define LED_IR_SIDE_L       0
-/** Channel used by side right IR LED. */
-#define LED_IR_SIDE_R       1
-/** Channel used by diagonal left IR LED. */
-#define LED_IR_DIAG_L       2
-/** Channel used by diagonal right IR LED. */
-#define LED_IR_DIAG_R       3
-/** Channel used by front left IR LED. */
+/** Side left IR LED id. */
+#define LED_IR_SIDE_L       3
+/** Side right IR LED id. */
+#define LED_IR_SIDE_R       2
+/** Diagonal left IR LED id. */
+#define LED_IR_DIAG_L       5
+/** Diagonal right IR LED id. */
+#define LED_IR_DIAG_R       1
+/** Front left IR LED id. */
 #define LED_IR_FRONT_L      4
-/** Channel used by front right IR LED. */
-#define LED_IR_FRONT_R      5
-/** Channel used by battery voltage LED. */
-#define LED_VBAT            6
+/** Front right IR LED id. */
+#define LED_IR_FRONT_R      0
+/** Battery voltage LED id. */
+#define LED_VBAT            12
 
 /**
  * Initialise LED controller to work.
@@ -28,17 +28,17 @@
 void led_init(void);
 
 /**
- * Turn on led on a given channel.
+ * Turn on led with a given id.
  *
- * @param channel           LED channel.
+ * @param id                LED id.
  */
-void led_on(int32_t channel);
+void led_on(int32_t id);
 
 /**
- * Turn off led on a given channel.
+ * Turn off led with a given id.
  *
- * @param channel           LED channel.
+ * @param id                LED id.
  */
-void led_off(int32_t channel);
+void led_off(int32_t id);
 
 #endif /* _LED_H_ */
