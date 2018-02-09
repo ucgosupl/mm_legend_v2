638261655293a84b560cbfeb691ccdb988f77769
Maciej Gajdzica <mgajdzica@gmail.com>
2018-02-10 00:09:16 +0100

    project: add incremental build for unit tests.

--- test/unit_test/cpputest/build.mk
+++ test/unit_test/cpputest/build.mk
@@ -12,12 +12,16 @@ include ../../../makefiles/platform.mk
 # File paths defines
 
 C_EXT := c
-C_SRC := $(wildcard $(addsuffix /*.$(C_EXT), $(SRC_DIRS)))
-OBJS := $(addprefix $(OUT_DIR), $(patsubst %.$(C_EXT),%.o,$(notdir $(C_SRC))))
+C_SRCS := $(wildcard $(addsuffix /*.$(C_EXT), $(SRC_DIRS)))
+OBJS := $(addprefix $(OUT_DIR), $(patsubst %.$(C_EXT),%.o,$(notdir $(C_SRCS))))
 
 CPP_EXT := cpp
-CPP_SRC := $(wildcard $(addsuffix /*.$(CPP_EXT), $(SRC_DIRS)))
-OBJS += $(addprefix $(OUT_DIR), $(patsubst %.$(CPP_EXT),%.o,$(notdir $(CPP_SRC))))
+CPP_SRCS := $(wildcard $(addsuffix /*.$(CPP_EXT), $(SRC_DIRS)))
+OBJS += $(addprefix $(OUT_DIR), $(patsubst %.$(CPP_EXT),%.o,$(notdir $(CPP_SRCS))))
+
+CXX_DEPS := $(addprefix $(OUT_DIR), $(notdir $(CXX_SRCS:.$(CXX_EXT)=.d)))
+C_DEPS := $(addprefix $(OUT_DIR), $(notdir $(C_SRCS:.$(C_EXT)=.d)))
+DEPS := $(CXX_DEPS) $(C_DEPS)
 
 VPATH := $(sort $(INC_DIRS) $(SRC_DIRS))
 
@@ -28,12 +32,12 @@ VPATH := $(sort $(INC_DIRS) $(SRC_DIRS))
 # Compiler and linker defines
 
 # Currently no special flags for CppUTest compilation
-C_FLAGS := -std=gnu89 -O0 -g
+C_FLAGS := -std=gnu89 -O0 -g -MMD
 C_INC_FLAGS := $(patsubst %, -I%, $(INC_DIRS))
 C_FLAGS += $(C_INC_FLAGS)
 
 # Currently no special flags for CppUTest compilation
-CPP_FLAGS := -std=gnu++11 -O0 -g
+CPP_FLAGS := -std=gnu++11 -O0 -g -MMD
 CPP_INC_FLAGS := $(patsubst %, -I%, $(INC_DIRS))
 CPP_FLAGS += $(CPP_INC_FLAGS)
 
@@ -59,7 +63,10 @@ $(OUT_DIR)%.o : %.$(C_EXT)
 	$(ECHO) 'Compiling file: $<'
 	$(CC) -c $(C_FLAGS) $< -o $@
 	$(ECHO) ' '
-	
+
+# header dependencies
+-include $(DEPS)
+
 out_dir :
 	$(MKDIR) $(subst /,,$(OUT_DIR))
 	$(MKDIR) $(OUT_DIR)lib
