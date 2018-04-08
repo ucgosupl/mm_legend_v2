8e939b99c46ea668bf4af99c5b811be6cfd25b99
Maciej Gajdzica <mgajdzica@gmail.com>
2018-04-08 02:27:37 +0200

    solver: add new functions for calculating paths to the center or to the start and returning next cell index.

--- src/code/solver/solver.h
+++ src/code/solver/solver.h
@@ -13,9 +13,14 @@
 void solver_init(void);
 
 /**
- * Calculate paths in the maze.
+ * Calculate paths to the center of the maze.
  */
-void solver_path_calc(void);
+void solver_calc_path_to_center(void);
+
+/**
+ * Calculate paths to the start of the maze.
+ */
+void solver_calc_path_to_start(void);
 
 /**
  * Get calculated cell value.
@@ -25,5 +30,14 @@ void solver_path_calc(void);
  * @return                  Callculated floodfill value for this cell.
  */
 uint32_t solver_cell_val_get(int32_t cell_id);
+
+/**
+ * Get next cell that should be visited from current cell.
+ *
+ * @param cell_id           Current cell id.
+ *
+ * @return                  Next cell that should be visited.
+ */
+int32_t solver_cell_id_next_get(int32_t cell_id);
 
 #endif /* _SOLVER_H_ */
